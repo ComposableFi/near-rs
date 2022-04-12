11e41a5cb7344bf4282f3a89c9a70bff9ed05311
Blas Rodriguez Irizar <rodrigblas@gmail.com>
2022-04-12 10:55:41 +0200

    add From conversion for BlockView <-> TrustedCheckpoint

--- near-lite-client/src/checkpoint.rs
+++ near-lite-client/src/checkpoint.rs
@@ -5,11 +5,24 @@
 //! there's also the possiblity to feed the client with a checkpoint. A checkpoint
 //! is a state of the chain that's considered **valid**.
 
-use crate::types::{Signature, SignatureType};
+use crate::types::{BlockProducer, BlockView, EpochId, ValidatorStakeView};
 
 pub struct TrustedCheckpoint {
-    pub epoch_id: Vec<u8>,
+    pub epoch_id: EpochId,
+    pub next_epoch_id: EpochId,
     pub height: u64,
-    pub next_bps: Vec<(SignatureType, Signature)>,
-    // TODO: add more fields
+    pub next_bps: Option<Vec<ValidatorStakeView>>,
+    pub approvals_after_next: Vec<Option<BlockProducer>>,
+}
+
+impl From<&TrustedCheckpoint> for BlockView {
+    fn from(v: &TrustedCheckpoint) -> Self {
+        Self {
+            epoch_id: v.epoch_id.clone(),
+            next_epoch_id: v.next_epoch_id.clone(),
+            height: v.height,
+            next_bps: v.next_bps.clone(),
+            approvals_after_next: v.approvals_after_next.clone(),
+        }
+    }
 }
