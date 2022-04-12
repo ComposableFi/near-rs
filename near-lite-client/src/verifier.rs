674ec7fe1e34e971beb99d8712045b4df8d39882
Blas Rodriguez Irizar <rodrigblas@gmail.com>
2022-04-12 10:55:20 +0200

    update validate_and_update_head to contain a block_view

--- near-lite-client/src/verifier.rs
+++ near-lite-client/src/verifier.rs
@@ -1,8 +1,8 @@
-use crate::{block_validation::BlockValidation, storage::StateStorage};
+use crate::{block_validation::BlockValidation, storage::StateStorage, types::BlockView};
 
 pub trait StateTransitionVerificator {
     type V: BlockValidation;
     type S: StateStorage;
 
-    fn validate_and_update_head(&mut self) -> bool;
+    fn validate_and_update_head(&mut self, block_view: &BlockView) -> bool;
 }
