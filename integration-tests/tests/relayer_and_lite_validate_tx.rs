65fce9b9002ecbbce4aa2b36842ceeb97eea62f6
Blas Rodriguez Irizar <rodrigblas@gmail.com>
2022-04-25 13:21:53 +0200

    clarify test docs

--- integration-tests/tests/relayer_and_lite_validate_tx.rs
+++ integration-tests/tests/relayer_and_lite_validate_tx.rs
@@ -1,9 +1,4 @@
-use std::{thread::park_timeout, time::Duration};
-
-use near_lite_relayer::{
-    blockchain_connector::{BlockchainConnector, NearNetwork},
-    state::LightClientState,
-};
+use near_lite_relayer::blockchain_connector::{BlockchainConnector, NearNetwork};
 
 use borsh::{BorshDeserialize, BorshSerialize};
 use near_lite_client::{
@@ -11,14 +6,11 @@ use near_lite_client::{
     TrustedCheckpoint,
 };
 
-/// ## Both Relayer and Lite Client
+/// ## Both Relayer and Lite Client - testing tx validation
 ///
-/// Note: we're testing happy path only here
-///
-/// Connects to Testnet using the relayer logic from `near-lite-relayer`
-/// Serializes the data, and feeds it into the light client
-/// Validates that the block is correct
-///
+/// Finds one transaction from the last 300 blocks
+/// Validates that the transaction is part of a valid block that the lite client has already
+/// validated.
 #[tokio::test]
 async fn both_relayer_and_lite_client_validate_tx() {
     env_logger::init();
@@ -40,7 +32,7 @@ async fn both_relayer_and_lite_client_validate_tx() {
         BorshDeserialize::try_from_slice(&serialized_block_view).unwrap();
 
     let trusted_checkpoint = TrustedCheckpoint(block_view_for_lite_client);
-    let mut lite_client = LightClient::new_from_checkpoint(trusted_checkpoint, 10);
+    let lite_client = LightClient::new_from_checkpoint(trusted_checkpoint, 10);
     // find a transaction in a block that has been validated
     let mut height = almost_latest_height - 500;
 
@@ -54,9 +46,6 @@ async fn both_relayer_and_lite_client_validate_tx() {
                 .get_transaction_ids_in_chunk(chunk_id)
                 .unwrap();
             if !tx_info.is_empty() {
-                // lite_client.validate_transaction(outcome_proof, )
-                dbg!(bs58::encode(&tx_info[0].0.try_to_vec().unwrap()).into_string());
-                dbg!(&tx_info[0].1);
                 let (tx_hash, sender_id) = tx_info[0].clone();
                 let tx_light_client_proof = blockchain_connector
                     .get_light_client_proof_transaction(almost_last_block_hash, tx_hash, sender_id)
@@ -100,10 +89,12 @@ async fn both_relayer_and_lite_client_validate_tx() {
                     .unwrap();
 
                 break 'l;
-            } else {
-                dbg!("NADA");
             }
         }
         height -= 1;
+        assert!(
+            almost_latest_height < height + 500 + 300,
+            "stopping the test - there should be at least one tx in 300 blocks"
+        );
     }
 }
